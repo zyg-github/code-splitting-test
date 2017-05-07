d01b27558a4fc7a0b4b0fcb7116be8a3306ca56d
zyg <1727970024@qq.com>
2017-05-07 15:09:46 +0800

    code-splitting-test
    code-splitting-test

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div><router-link to="/hello">Hello</router-link></div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  name: 'app',
+  created () {
+    axios.get('https://www.baidu.com/')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
